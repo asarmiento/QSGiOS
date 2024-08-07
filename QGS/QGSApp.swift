1ccd6134aa2fb3405507329d9ca9d6978fbdc1fb
Anwar Sarmiento <anwarsarmiento@gmail.com>
2024-08-07 07:57:58 -0400

    ajuste de session

--- QGS/QGSApp.swift
+++ QGS/QGSApp.swift
@@ -3,18 +3,27 @@
 //  QGS
 //
 //  Created by Edin Martinez on 7/24/24.
-//
+//  
 
 import SwiftUI
 
 @main
 struct QGSApp: App {
-    
+     let accessToken = UserDefaults.standard.string(forKey: "accessToken")
     @State private var locationH = LocationManager()
     var body: some Scene {
         WindowGroup {
-            Login()
+            if accessToken != nil {
+                HomeRecord()
+            }else{
+                Login()
+            }
             
         }.modelContainer(for: [UserModel.self,RecordModel.self])
     }
+    
+  
 }
+
+
+

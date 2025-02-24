061e3b6f96ed484d1b38536049ae5cacd480f120
Anwar Sarmiento Ramos <anwarsarmiento@gmail.com>
2025-02-24 11:42:27 -0500

    fix
    cambio en colores

--- QGS/View/ListRecordDetails.swift
+++ QGS/View/ListRecordDetails.swift
@@ -19,55 +19,65 @@ struct ListRecordDetails: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                Color.clear.edgesIgnoringSafeArea(.all)
-                HeadSecondary(title: "Detalle de Horas diarios", showPDFView: $showPDFView, showTimeRecordView: $showTimeRecordView)
-                VStack{
-                    if networkListDetails.isLoading {
-                        ProgressView("Cargando...")
-                            .progressViewStyle(CircularProgressViewStyle())
-                    } else if let errorMessage = networkListDetails.errorMessage {
-                        Text(errorMessage)
-                            .foregroundColor(.red)
-                    } else {
-                        if !networkListDetails.workEntries.isEmpty {
-                            List(networkListDetails.workEntries) { workEntry in
-                                VStack(alignment: .leading) {
-                                    Text("Proyecto: \(workEntry.project.name)")
-                                        .font(.headline)
-                                    HStack {
-                                        Text("Fecha: \(workEntry.date)")
-                                        Text("Horas: \(workEntry.hours != nil ? String(format: "%.2f", workEntry.hours!) : "N/A")")
-                                    }
-                                    
-                                    Text("Hora de Registro: \(workEntry.time)")
-                                    Text("Tipo: \(workEntry.type)")
-                                        .font(.subheadline)
-                                        .foregroundColor(.gray)
-                                }
-                                .padding(1)
-                            }
+      
+                VStack {
+                    
+                    HeadSecondary(title: "Detalle de Horas diarios", showPDFView: $showPDFView, showTimeRecordView: $showTimeRecordView)
+                    VStack{
+                        if networkListDetails.isLoading {
+                            ProgressView("Cargando...")
+                                .progressViewStyle(CircularProgressViewStyle())
+                        } else if let errorMessage = networkListDetails.errorMessage {
+                            Text(errorMessage)
+                                .foregroundColor(.red)
                         } else {
-                            Text("No hay entradas y salidas registradas")
-                                .foregroundColor(.gray)
-                                .font(.title)
+                            if !networkListDetails.workEntries.isEmpty {
+                                ZStack {
+                                    Color.white.edgesIgnoringSafeArea(.all)  // Fondo blanco general
+                                    List(networkListDetails.workEntries) { workEntry in
+                                        VStack(alignment: .leading) {
+                                            Text("Proyecto: \(workEntry.project.name)")
+                                                .font(.headline)
+                                                .foregroundColor(.black)
+                                            HStack {
+                                                Text("Fecha: \(workEntry.date)")
+                                                    .foregroundColor(.black)
+                                                Text("Horas: \(workEntry.hours != nil ? String(format: "%.2f", workEntry.hours!) : "N/A")")
+                                                    .foregroundColor(.black)
+                                            }
+                                            Text("Hora de Registro: \(workEntry.time)")
+                                                .foregroundColor(.black)
+                                            Text("Tipo: \(workEntry.type)")
+                                                .font(.subheadline)
+                                                .foregroundColor(.gray)
+                                        }
+                                        .padding(1)
+                                        .listRowBackground(Color.white)       // Fondo blanco para la fila
+                                    }
+                                    .scrollContentBackground(.hidden)
+                                    .background(Color.white)                 // Fondo de la lista
+                                }
+                            } else {
+                                Text("No hay entradas y salidas registradas")
+                                    .foregroundColor(.gray)
+                                    .font(.title)
+                            }
                         }
                     }
-                }
-                .frame(height: 600)
-                .offset(y: -50)
-                .navigationDestination(isPresented: $showPDFView) {
-                    PDFView(url: URL(string: "https://api.friendlypayroll.net/weekly-hours")!)
-                }
-                .navigationDestination(isPresented: $showTimeRecordView) {
-                    TimeRecordsView()
-                }
-                .onAppear {
-                    networkListDetails.context = modelContext
-                    networkListDetails.fetchWorkEntries()
-                }}
+                    .frame(height: 600)
+                    .offset(y: -50)
+                    .navigationDestination(isPresented: $showPDFView) {
+                        PDFView(url: URL(string: "https://api.friendlypayroll.net/weekly-hours")!)
+                    }
+                    .navigationDestination(isPresented: $showTimeRecordView) {
+                        TimeRecordsView()
+                    }
+                    .onAppear {
+                        networkListDetails.context = modelContext
+                        networkListDetails.fetchWorkEntries()
+                    }}
+            }
         }
-    }
 }
 
 
